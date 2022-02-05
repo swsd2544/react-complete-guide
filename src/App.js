f90dcdc65d41ef3a1117142a8256e419d3d4bcfa
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2022-02-05 20:21:49 +0700

    module-14: Sending Http Requests

--- src/App.js
+++ src/App.js
@@ -1,28 +1,86 @@
-import { useState } from 'react';
-import Cart from './components/Cart/Cart';
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import React, { useState, useEffect, useCallback } from 'react';
+
+import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
+import './App.css';
 
 function App() {
-	const [cartIsShown, setCartIsShown] = useState(false);
+	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
 
-	const showCartHandler = () => {
-		setCartIsShown(true);
-	};
+	const fetchMoviesHandler = useCallback(async () => {
+		setIsLoading(true);
+		setError(null);
+		try {
+			const response = await fetch(
+				'https://react-http-9aa96-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json'
+			);
+			if (!response.ok) {
+				throw new Error('Something went wrong!');
+			}
 
-	const hideCartHandler = () => {
-		setCartIsShown(false);
-	};
+			const data = await response.json();
+
+			const loadedMovies = [];
+			for (const key in data) {
+				loadedMovies.push({
+					id: key,
+					title: data[key].title,
+					openingText: data[key].openingText,
+					releaseDate: data[key].releaseDate,
+				});
+			}
+			setMovies(loadedMovies);
+		} catch (error) {
+			setError(error.message);
+		}
+		setIsLoading(false);
+	}, []);
+
+	useEffect(() => {
+		fetchMoviesHandler();
+	}, [fetchMoviesHandler]);
+
+	async function addMovieHandler(movie) {
+		const response = await fetch(
+			'https://react-http-9aa96-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json',
+			{
+				method: 'POST',
+				body: JSON.stringify(movie),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		);
+		const data = await response.json();
+		console.log(data);
+	}
+
+	let content = <p>Found no movies.</p>;
+
+	if (movies.length > 0) {
+		content = <MoviesList movies={movies} />;
+	}
+
+	if (error) {
+		content = <p>{error}</p>;
+	}
+
+	if (isLoading) {
+		content = <p>Loading...</p>;
+	}
 
 	return (
-		<CartProvider>
-			{cartIsShown && <Cart onHideCart={hideCartHandler} />}
-			<Header onShowCart={showCartHandler} />
-			<main>
-				<Meals />
-			</main>
-		</CartProvider>
+		<React.Fragment>
+			<section>
+				<AddMovie onAddMovie={addMovieHandler} />
+			</section>
+			<section>
+				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
+			</section>
+			<section>{content}</section>
+		</React.Fragment>
 	);
 }
 
