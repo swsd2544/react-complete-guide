8d1960f550de2663d562513608ede96f1d50286c
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2022-02-05 21:36:51 +0700

    module-15: Building Custom React Hooks (part 1)

---
+++ src/components/ForwardCounter.js
@@ -0,0 +1,13 @@
+import useCounter from '../hooks/use-counter';
+
+import Card from './Card';
+
+const counterFn = (prev) => prev + 1;
+
+const ForwardCounter = () => {
+	const counter = useCounter(counterFn);
+
+	return <Card>{counter}</Card>;
+};
+
+export default ForwardCounter;
