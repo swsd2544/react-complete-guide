4ca6b19f3e484846a6f0966185b9091add759c03
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2022-03-27 23:04:43 +0700

    module-18: Diving into Redux

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
-import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import Cart from './components/Cart/Cart';
-import CartProvider from './store/CartProvider';
+import Headers from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
+	const isAuthenticated = useSelector(({ auth }) => auth.isAuthenticated);
 
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
-
-  const hideCartHandler = () => {
-    setCartIsShown(false);
-  };
-
-  return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <main>
-        <Meals />
-      </main>
-    </CartProvider>
-  );
+	return (
+		<>
+			<Headers />
+			{!isAuthenticated && <Auth />}
+			{isAuthenticated && <UserProfile />}
+			<Counter />
+		</>
+	);
 }
 
 export default App;
